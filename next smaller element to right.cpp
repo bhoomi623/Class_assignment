fa93e0a2cca7b9b8205b26bdfdf67f85cbd39e69
bhoomi623 <120084156+bhoomi623@users.noreply.github.com>
2023-04-13 03:33:27 +0530

    Created

---
+++ next smaller element to right.cpp
@@ -0,0 +1,14 @@
+vector<int> nextSmallestElementToRight(vector<int> arr, int n) 
+{ 
+ vector<int> ans(n,-1);
+ stack<int> st;
+ st.push(-1);
+ for(int i=n-1;i>=0;i--)
+ {
+  while(st.top()>=arr[i])
+      st.pop();
+  ans[i]=st.top();
+  st.push(arr[i]);
+ }
+  return ans;
+}
