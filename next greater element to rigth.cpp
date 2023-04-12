f56b72709f8334aafe04bc7f228c8159673f0cc6
bhoomi623 <120084156+bhoomi623@users.noreply.github.com>
2023-04-13 03:28:04 +0530

    Created

---
+++ next greater element to rigth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        vector<int> right(vector<int>& nums) {
+            vector<int> ans;
+            stack<int> st;
+            for(int i = nums.size() - 1; i >= 0; i--){
+                while(st.empty() && st.top() >= nums[i])
+                    st.pop();
+                if(st.empty())
+                    ans.push_back(-1);
+                else
+                    ans.push_back(st.top());
+                st.push(nums[i]);
+            }
+            return ans;
+        }
+    };
