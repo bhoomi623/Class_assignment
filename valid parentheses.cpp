577407052a8c49f1c8131352051af55ab6c5b5d0
bhoomi623 <120084156+bhoomi623@users.noreply.github.com>
2023-04-13 03:30:33 +0530

    created

---
+++ valid parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        bool isValid(string str){
+            stack<char> st;
+            for(auto ch : str) {
+                if(ch == '(' || ch == '{' || ch == '[')
+                    st.push(ch);
+                else
+                    if(st.empty() || (ch == ')' && st.top() != '(') || 
+                    (ch == '}' && st.top() != '{') || (ch == ']' && st.top() != '['))
+                        return false;
+                    st.pop();
+                }
+            }
+            return st.empty();
+        }
+    };
